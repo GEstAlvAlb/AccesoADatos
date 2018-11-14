4464fea317278be61091b1aa85ea8b14f10614cd
alberto <alberto.a.e.g.m@hotmail.com>
2018-11-14 20:33:01 +0100

    segunda

---
+++ ejercicios/loginProfeso/src/LoginProfesor/src/com/sfaci/login/Programa.java
@@ -0,0 +1,13 @@
+package LoginProfesor.src.com.sfaci.login;
+
+import LoginProfesor.src.com.sfaci.login.ui.Vista;
+
+public class Programa {
+
+	public static void main(String args[]) {
+		
+		Vista vista = new Vista();
+		Modelo modelo = new Modelo();
+		Controlador controlador = new Controlador(modelo, vista);
+	}
+}
